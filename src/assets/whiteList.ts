e9427086d6094f70c446524f946410f8ae46456f
codehans <94654388+codehans@users.noreply.github.com>
2021-11-25 11:02:02 +0000

    add kuji

--- src/assets/whiteList.ts
+++ src/assets/whiteList.ts
@@ -49,6 +49,7 @@ export const whiteList = {
     terra1e59utusv5rspqsu8t37h5w887d9rdykljedxw0: 'VKR-UST',
     terra1z6tp0ruxvynsx5r9mmcc2wcezz9ey9pmrw5r8g: 'ORION-UST',
     terra170hht9lwe50ugfhnruzjaezpj29l7nvw3l6725: 'ORION-LUNA',
+    terra1zkyrfyq7x9v5vqnnrznn3kvj35az4f6jxftrl2: 'KUJI-UST',
   },
   token: {
     uusd: 'UST',
@@ -100,5 +101,6 @@ export const whiteList = {
     terra19djkaepjjswucys4npd5ltaxgsntl7jf0xz7w6: 'TWD',
     terra1dy9kmlm4anr92e42mrkjwzyvfqwz66un00rwr5: 'VKR',
     terra1mddcdx0ujx89f38gu7zspk2r2ffdl5enyz2u03: 'ORION',
+    terra1xfsdgcemqwxp4hhnyk4rle6wr22sseq7j07dnn: 'KUJI',
   }
 }
